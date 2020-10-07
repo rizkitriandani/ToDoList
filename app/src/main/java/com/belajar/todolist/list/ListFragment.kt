e65b2238d02dc5298c287f033667a6ba173c3c31
rizkitriandani <rizkitriandani@yahoo.co.id>
2020-10-07 21:25:11 +0700

    Update layout and menu for list fragment added.

--- app/src/main/java/com/belajar/todolist/list/ListFragment.kt
+++ app/src/main/java/com/belajar/todolist/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.belajar.todolist.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.belajar.todolist.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
@@ -24,7 +22,13 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_addFragment);
         }
 
+        setHasOptionsMenu(true)
+
         return view;
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+      inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 
 }
